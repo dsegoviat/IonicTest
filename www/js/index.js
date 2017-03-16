d8d84aebc82e32e4f85776932cbe55e6bc2d9f07
MSI Barcelona <j.espasa.cusachs@accenture.com>
2017-03-16 10:31:52 +0100

    File system app

--- www/js/index.js
+++ www/js/index.js
@@ -5,87 +5,169 @@
 // the 2nd parameter is an array of 'requires'
 angular.module('starter', ['ionic'])
 
-.run(function($ionicPlatform) {
-  $ionicPlatform.ready(function() {
-    if(window.cordova && window.cordova.plugins.Keyboard) {
-      // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
-      // for form inputs)
-      cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+  .run(function ($ionicPlatform) {
+    $ionicPlatform.ready(function () {
+      if (window.cordova && window.cordova.plugins.Keyboard) {
+        // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
+        // for form inputs)
+        cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
 
-      // Don't remove this line unless you know what you are doing. It stops the viewport
-      // from snapping when text inputs are focused. Ionic handles this internally for
-      // a much nicer keyboard experience.
-      cordova.plugins.Keyboard.disableScroll(true);
-    }
-    if(window.StatusBar) {
-      StatusBar.styleDefault();
-    }
-  });
-
-  document.addEventListener("deviceready", onDeviceReady, false);
-
-  function setLocalStorage() {
-    localStorage.setItem("name", "David");
-    localStorage.setItem("job", "Developer");
-    localStorage.setItem("project", "Cordova Project");
-  }
-
-  function showLocalStorage() {
-    console.log(localStorage.getItem("name"));
-    console.log(localStorage.getItem("job"));
-    console.log(localStorage.getItem("project"));
-  }
-
-  function removeProjectFromLocalStorage() {
-    localStorage.removeItem("project");
-  }
-
-  function getLocalStorageByKey() {
-    console.log(localStorage.key(0));
-  }
-
-  function onDeviceReady() {
-    document.getElementById("setLocalStorage").addEventListener(
-      "click",
-      setLocalStorage
-    );
-
-    document.getElementById("showLocalStorage").addEventListener(
-      "click",
-      showLocalStorage
-    );
-
-    document.getElementById("removeProjectFromLocalStorage").addEventListener(
-      "click",
-      removeProjectFromLocalStorage
-    );
-
-    document.getElementById("getLocalStorageByKey").addEventListener(
-      "click",
-      getLocalStorageByKey
-    );
-
-    document.getElementById("takePicture").addEventListener(
-      "click",
-      cameraTakePicture
-    );
-  }
-
-  function cameraTakePicture() {
-    console.log("Take a picture pressed");
-    navigator.camera.getPicture(onSuccess, onFail, {
-      quality: 50,
-      destinationType: Camera.DestinationType.DATA_URL
+        // Don't remove this line unless you know what you are doing. It stops the viewport
+        // from snapping when text inputs are focused. Ionic handles this internally for
+        // a much nicer keyboard experience.
+        cordova.plugins.Keyboard.disableScroll(true);
+      }
+      if (window.StatusBar) {
+        StatusBar.styleDefault();
+      }
     });
 
-    function onSuccess(imageData) {
-      var image = document.getElementById('myImage');
-      image.src = "data:image/jpeg;base64," + imageData;
+    document.addEventListener("deviceready", onDeviceReady, false);
+
+    function createFile() {
+      var type = window.TEMPORARY;
+      var size = 5 * 1024 * 1024;
+
+      window.requestFileSystem(type, size, successFileSystem, failFileSystem);
+
+      function successFileSystem(fs) {
+        fs.root.getFile('log.txt', {create: true, exclusive: true}, function (fileEntry) {
+          alert('File creation successfull!');
+        }, failFileSystem);
+      }
+
+      function failFileSystem(error) {
+        var msg = '';
+        switch (error.code) {
+          case FileError.QUOTA_EXCEEDED_ERR:
+            msg = 'QUOTA_EXCEEDED_ERR';
+            break;
+          case FileError.NOT_FOUND_ERR:
+            msg = 'NOT_FOUND_ERR';
+            break;
+          case FileError.SECURITY_ERR:
+            msg = 'SECURITY_ERR';
+            break;
+          case FileError.INVALID_MODIFICATION_ERR:
+            msg = 'INVALID_MODIFICATION_ERR';
+            break;
+          case FileError.INVALID_STATE_ERR:
+            msg = 'INVALID_STATE_ERR';
+            break;
+          default:
+            msg = 'Files has already been created';
+            break;
+        }
+        alert('Error: ' + msg);
+      }
     }
 
-    function onFail(message) {
-      alert('Failed because: ' + message);
-    }
-  }
+    function writeFile() {
+      var type = window.TEMPORARY;
+      var size = 5*1024*1024;
 
-});
+      window.requestFileSystem(type, size, successCallback, errorCallback);
+
+      function successCallback(fs) {
+
+        fs.root.getFile('log.txt', {create: true}, function (fileEntry) {
+          fileEntry.createWriter(function(fileWriter) {
+            fileWriter.onwriteend = function(e) {
+              alert('Write completed.');
+            };
+
+            fileWriter.onerror = function(e) {
+              alert('Write failed: ' + e.toString());
+            };
+
+            var blob = new Blob(['Lorem Ipsum'], {type: 'text/plain'});
+            fileWriter.write(blob);
+          }, errorCallback);
+        }, errorCallback);
+      }
+
+      function errorCallback(error) {
+        alert('Error: ' + error.code);
+      }
+    }
+
+    function readFile() {
+      var type = window.TEMPORARY;
+      var size = 5*1024*1024;
+
+      window.requestFileSystem(type, size, successCallback, errorCallback);
+
+      function successCallback(fs) {
+
+        fs.root.getFile('log.txt', {}, function (fileEntry) {
+          fileEntry.file(function (file) {
+            var reader = new FileReader();
+
+            reader.onloadend = function (e) {
+              var textArea = document.getElementById('textArea');
+              textArea.value = this.result;
+            };
+
+            reader.readAsText(file);
+
+
+          }, errorCallback);
+        }, errorCallback);
+
+      }
+
+      function errorCallback(error) {
+        var textArea = document.getElementById('textArea');
+        textArea.value = 'File does not exist.';
+        alert('Error: ' + error.message);
+      }
+
+    }
+
+    function deleteFile() {
+
+      var type = window.TEMPORARY;
+      var size = 5*1024;
+
+      window.requestFileSystem(type, size, successCallback, errorCallback);
+
+      function successCallback(fs) {
+
+        fs.root.getFile('log.txt', {create: false}, function (fileEntry) {
+          fileEntry.remove(function () {
+            alert('Removed successfully');
+          }, errorCallback)
+        }, errorCallback)
+
+      }
+
+      function errorCallback(error) {
+        alert(('Error: ' + error.message));
+      }
+
+    }
+
+    function onDeviceReady() {
+      document.getElementById("createFile").addEventListener(
+        "click",
+        createFile
+      );
+
+      document.getElementById("writeFile").addEventListener(
+        "click",
+        writeFile
+      );
+
+      document.getElementById("readFile").addEventListener(
+        "click",
+        readFile
+      );
+
+      document.getElementById("deleteFile").addEventListener(
+        "click",
+        deleteFile
+      );
+    }
+
+
+  });
